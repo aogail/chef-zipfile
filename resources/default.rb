3771ea1e31a99e735232f4fa9bcfcd753dcc65fb
Christoph Jahn (chn) <christoph.jahn@softwareag.com>
2016-07-27 08:40:52 +0200

    #1 Support overwrite of existing files

--- resources/default.rb
+++ resources/default.rb
@@ -3,3 +3,4 @@ default_action :extract
 
 attribute :from, :kind_of => String, :name_attribute => true
 attribute :into, :kind_of => String, :required => true
+attribute :overwrite, :kind_of => [ TrueClass, FalseClass ], :default => false
