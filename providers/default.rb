132d0505ac7ca33412f9328c383fa56978d5ea8c
Mattia Rossi <mattia.rossi@softsolutions.it>
2017-11-07 12:45:37 +0100

    Fix #10: workaround for handle unsorted zip entry.

--- providers/default.rb
+++ providers/default.rb
@@ -10,6 +10,10 @@ action :extract do
   converge_by "Extracting #{new_resource.from} into #{new_resource.into}" do
     ::Zip::File.open(new_resource.from) do |zip|
       zip.each do |zipentry|
+        folder = ::File.join(new_resource.into, ::File.dirname(zipentry.name))
+        unless ::Dir.exist?(folder)
+          ::FileUtils.mkpath folder
+        end
         destination = ::File.join(new_resource.into, zipentry.name)
         zip.extract(zipentry, destination)
       end
@@ -25,4 +29,5 @@ def install_rubyzip_gem
     compile_time true
   end
   require 'zip'
+  require 'fileutils'
 end
